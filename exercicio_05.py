dc0d02a9ac864d7903fe4ff71cafb9675ff13371
Bernardo Guerra <bernardoguerra93@gmail.com>
2023-09-15 11:23:21 -0300

    fiz o quinto exercício

--- exercicio_05.py
+++ exercicio_05.py
@@ -1,7 +1,14 @@
-n=0
+numero=0
+soma=0
+numeropositivo=0
 for x in range(0, 10, 1):
-    f=int(input("até que números vamos contar? "))
-    if f<0:
-        n= n+1
-print(n)
-
+    negativo=int(input("Que números? "))
+    if negativo<0:
+        print(negativo, "é negativo")
+        numero= numero + 1
+        soma= soma + negativo
+    else:
+        numeropositivo= numeropositivo +1
+print(f"a soma dos números é {soma}")
+print(numero, f"números negativos")
+print(numeropositivo, "numero positivos")
